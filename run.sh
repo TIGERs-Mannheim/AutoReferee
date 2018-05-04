2740621d22248408fa112ddcb378721a1f53dab1
Nicolai Ommer <nicolai.ommer@qaware.de>
2018-05-04 08:49:16 +0200

    Add an option to scale the font size

--- run.sh
+++ run.sh
@@ -22,6 +22,7 @@ mvn -pl modules/autoreferee-main exec:exec -Dmaven.repo.local=repository \
     -Dexec.args="-Dautoref.mode=$mode $autoRecording \
     -Xms128m -Xmx1G -server -Xnoclassgc -Xverify:none -Dsun.java2d.d3d=false -XX:+UseG1GC -Djava.net.preferIPv4Stack=true -XX:-OmitStackTraceInFastThrow \
     -classpath %classpath \
+    -Dswing.plaf.metal.controlFont='Arial-16' -Dswing.plaf.metal.userFont='Arial-16' \
     edu.tigers.autoref.AutoReferee"
 
 if [ "$?" != "0" ]; then
