b924d16e6c93ac1b7ab6dad9bc64486583844e10
Nicolai Ommer <nicolai.ommer@qaware.de>
2018-05-06 13:40:03 +0200

    Revert font scaling again and add it as a comment to the run script
    Rather prefer the default font settings by default, which works for must setups.

--- run.sh
+++ run.sh
@@ -17,12 +17,15 @@ fi
 # recording has a significant memory impact (both heap size and disk space).
 autoRecording="-Dproductive=false"
 
+# if the font size is too small (e.g. on high resolution screens), you can add following arguments to the command below:
+# -Dswing.plaf.metal.controlFont='sans-serif-16' -Dswing.plaf.metal.userFont='sans-serif-16' \
+# 16 is the font size and can be adapted. You can also choose other font types.
+
 mvn -pl modules/autoreferee-main exec:exec -Dmaven.repo.local=repository \
     --no-snapshot-updates \
     -Dexec.args="-Dautoref.mode=$mode $autoRecording \
     -Xms128m -Xmx1G -server -Xnoclassgc -Xverify:none -Dsun.java2d.d3d=false -XX:+UseG1GC -Djava.net.preferIPv4Stack=true -XX:-OmitStackTraceInFastThrow \
     -classpath %classpath \
-    -Dswing.plaf.metal.controlFont='Arial-16' -Dswing.plaf.metal.userFont='Arial-16' \
     edu.tigers.autoref.AutoReferee"
 
 if [ "$?" != "0" ]; then
