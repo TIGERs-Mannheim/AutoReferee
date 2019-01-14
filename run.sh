23734bc91e302eea0fb9cc83583100b8d59c62fa
Nicolai Ommer <nicolai.ommer@qaware.de>
2019-01-14 23:30:18 +0100

    Replace maven-exec plugin by direct call to java in run.sh script

--- run.sh
+++ run.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-EXEC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-if ! type mvn &> /dev/null; then
-	echo "Please add the maven executable to your \$PATH"
-	exit 1
-fi
-
-if [ ! -d "repository" ]; then
-    ./build.sh
+if [[ ! -d "modules/autoreferee-main/target" ]]; then
+    echo "Make sure to run ./build.sh first! It must also be executed after a code update or you will run with the old version."
+    exit 1
 fi
 
 mode="`echo $1 | tr '[:lower:]' '[:upper:]'`"
-if [ "$mode" != "ACTIVE" ] && [ "$mode" != "PASSIVE" ]; then
+if [[ "$mode" != "ACTIVE" ]] && [[ "$mode" != "PASSIVE" ]]; then
     echo "Please specify a valid mode to start with: active | passive"
     exit 1
 fi
@@ -21,19 +15,14 @@ fi
 # recording has a significant memory impact (both heap size and disk space). You should increase Xmx below to 4G.
 autoRecording="-Dproductive=false"
 
+args="$@"
+
+export CLASSPATH="modules/autoreferee-main/target/lib/*"
+
 # if the font size is too small (e.g. on high resolution screens), you can add following arguments to the command below:
 # -Dswing.plaf.metal.controlFont='sans-serif-16' -Dswing.plaf.metal.userFont='sans-serif-16' \
 # 16 is the font size and can be adapted. You can also choose other font types.
 
-mvn -pl modules/autoreferee-main exec:exec -Dmaven.repo.local=repository \
-    --no-snapshot-updates \
-    -Dexec.args="-Dautoref.mode=$mode $autoRecording \
-    -Xms128m -Xmx1G -server -Xnoclassgc -Xverify:none -Dsun.java2d.d3d=false -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -XX:-OmitStackTraceInFastThrow -XX:+AggressiveOpts \
-    -classpath %classpath \
-    edu.tigers.autoref.AutoReferee"
+JAVA_OPTS="-Xms64m -Xmx4G -server -Xnoclassgc -Xverify:none -Dsun.java2d.d3d=false -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -XX:-OmitStackTraceInFastThrow -XX:+AggressiveOpts $autoRecording -Dautoref.mode=$mode"
 
-if [ "$?" != "0" ]; then
-	echo
-	echo
-	echo "Launching the program failed. Did you build the application using './build.sh'?'"
-fi
+java ${JAVA_OPTS} edu.tigers.autoref.AutoReferee ${args}
