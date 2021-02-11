195ec62c558526223ba60e056957aab57956a3d5
Nicolai Ommer <nicolai.ommer@gmail.com>
2021-02-11 21:23:55 +0100

    Add docker hub trigger to pipeline

---
+++ .github/workflows/trigger_docker.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+for url in $(echo "$BUILD_TRIGGERS" | sed "s/,/ /g"); do
+  curl -X POST -H "Content-Type: application/json" --data "{ \"build\": true, \"source_name\": \"$GITHUB_REF\" }" "$url"
+done
