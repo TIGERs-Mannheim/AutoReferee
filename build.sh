2d8647c578e4b1571274da9c0a65863fbd82c5a7
Nicolai Ommer <nicolai.ommer@gmail.com>
2021-07-03 19:24:30 +0200

    Only use gradle to run AutoRef

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
 ./gradlew build -x test
-rm -rf autoReferee
-unzip build/distributions/autoReferee.zip
--- run.sh
+++ run.sh
@@ -2,13 +2,4 @@
 
 ARGS="$@"
 
-
-if [[ ! -d autoReferee ]]; then
-  ./gradlew run --args="${ARGS}"
-else
-  # if the font size is too small (e.g. on high resolution screens), you can add following arguments to the command below.
-  # 16 is the font size and can be adapted. You can also choose other font types.
-  # export AUTO_REFEREE_OPTS="-Dswing.plaf.metal.controlFont='sans-serif-16' -Dswing.plaf.metal.userFont='sans-serif-16'"
-
-  autoReferee/bin/autoReferee ${ARGS}
-fi
+./gradlew run --args="${ARGS}"
