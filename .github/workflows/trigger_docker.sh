cfe7890a674f18e72cfc38730b2dafdead0eb11b
Nicolai Ommer <nicolai.ommer@gmail.com>
2022-07-22 21:42:58 +0200

    Remove docker hub trigger
    This does not work anymore with a free subscription.

--- .github/workflows/trigger_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-for url in $(echo "$BUILD_TRIGGERS" | sed "s/,/ /g"); do
-  curl -X POST -H "Content-Type: application/json" --data "{ \"build\": true, \"source_name\": \"${GITHUB_REF#refs/*/}\" }" "$url"
-done
