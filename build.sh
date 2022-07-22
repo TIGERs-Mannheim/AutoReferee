a0bedc732918935ac5df7ad14567b2b486691aa8
Nicolai Ommer <nicolai.ommer@gmail.com>
2022-07-22 21:11:53 +0200

    Improve build and run scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-./gradlew build -x test
+./gradlew distZip
+rm -rf autoReferee
+unzip build/distributions/autoReferee.zip
--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-ARGS="$@"
+ARGS="$*"
 
-./gradlew run --args="${ARGS}"
+if [[ -d "autoReferee" ]]; then
+  echo "Starting prebuild distribution"
+  cd autoReferee && bin/autoReferee
+else
+  echo "Build and run"
+  if [[ -z "${ARGS}" ]]; then
+    ./gradlew :run
+  else
+    ./gradlew :run --args="${ARGS}"
+  fi
+fi
