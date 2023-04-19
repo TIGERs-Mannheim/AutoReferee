a6a4ab6e470ed974e986cd1d474279d340fcee81
Nicolai Ommer <nicolai.ommer@gmail.com>
2023-04-19 21:32:19 +0200

    Build auto-referee-vnc image

--- src/main/docker-vnc/docker-entry.sh
+++ src/main/docker-vnc/docker-entry.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-args="$@"
+args=("$@")
 
 if [[ "$1" == "vnc" ]]; then
   echo "Launch in VNC mode"
-  args="${@:1}"
+  args=("${@:1}")
   if [[ -z "${VNC_PASSWORD}" ]]; then
     VNC_PASSWORD=vncpassword
   fi
@@ -17,16 +17,15 @@ if [[ "$1" == "vnc" ]]; then
 
   cat <<EOF >>~/.xinitrc
 #!/bin/sh
-exec java -cp "/app/resources:/app/classes:/app/libs/*" "edu.tigers.autoref.AutoReferee" -w "${VNC_GEOMETRY}" $args > /tmp/out.log
+exec java -cp "/app/resources:/app/classes:/app/libs/*" "edu.tigers.autoref.AutoReferee" -w "${VNC_GEOMETRY}" "${args[@]}" > /tmp/out.log
 EOF
   chmod 700 ~/.xinitrc
-  
+
   touch /tmp/out.log
   tail -f /tmp/out.log &
 
   export X11VNC_CREATE_GEOM="${VNC_GEOMETRY}"
   exec x11vnc -forever -usepw -display WAIT:cmd=FINDCREATEDISPLAY-Xvfb
 else
-  echo "Launch in headless mode"
-  exec java -cp "/app/resources:/app/classes:/app/libs/*" "edu.tigers.autoref.AutoReferee" -hl $args
+  exec java -cp "/app/resources:/app/classes:/app/libs/*" "edu.tigers.autoref.AutoReferee" "${args[@]}"
 fi
