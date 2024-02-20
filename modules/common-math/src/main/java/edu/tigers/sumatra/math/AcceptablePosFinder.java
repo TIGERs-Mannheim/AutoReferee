3c191fc710e848c6fc5dfac93952a3e388a6d0cb
NicolaiO <nicolai.ommer@gmail.com>
2024-02-20 07:26:35 +0000

    Resolve "Path planning gets stuck during ball placement"
    Closes #1925
    
    See merge request main/Sumatra!1798
    
    sumatra-commit: b64ab2824cf3f3a169bf180be0c4285ad31d1a69

---
+++ modules/common-math/src/main/java/edu/tigers/sumatra/math/AcceptablePosFinder.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2009 - 2024, DHBW Mannheim - TIGERs Mannheim
+ */
+
+package edu.tigers.sumatra.math;
+
+import edu.tigers.sumatra.math.circle.Circle;
+import edu.tigers.sumatra.math.circle.ICircle;
+import edu.tigers.sumatra.math.vector.IVector2;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+
+
+@RequiredArgsConstructor
+public class AcceptablePosFinder
+{
+	private final double distanceStepSize;
+	private final int maxIterations;
+
+
+	public Optional<IVector2> findAcceptablePos(IVector2 start, Predicate<IVector2> check)
+	{
+		if (check.test(start))
+		{
+			return Optional.of(start);
+		}
+
+		int iteration = 0;
+		for (double radius = distanceStepSize; radius < 10_000; radius += distanceStepSize)
+		{
+			ICircle circle = Circle.createCircle(start, radius);
+			double perimeterLength = circle.getPerimeterLength();
+			for (double c = 0; c < perimeterLength; c += distanceStepSize)
+			{
+				IVector2 point = circle.stepAlongPath(c);
+				if (check.test(point))
+				{
+					return Optional.of(point);
+				}
+				iteration++;
+				if (iteration >= maxIterations)
+				{
+					return Optional.empty();
+				}
+			}
+		}
+		throw new IllegalStateException("No acceptable position found after " + iteration + " iterations.");
+	}
+}
